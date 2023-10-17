b932ff1b9486fca086d7bd6ea6a305c703d4a0a1
wally 🧨🎇 <ricardoborges21122006@gmail.com>
2023-10-16 22:46:05 -0300

    quase finalizado

--- app.js
+++ app.js
@@ -1,97 +1,93 @@
-let pontuacao = 0;
+let pontuacao = 0
+let questionIndex = 0
+const usedQuestions = new Set()
+const mostrarPergunta = document.querySelector('.container')
+let questoes
+let quizEmAndamento = false
+let tempoInicio = 0
+let tempoPassado = 0
+let timerId
 
-// Função para iniciar o quiz
 function startQuiz() {
-    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
-        .then(response => response.json())
-        .then(data => {
-            const questoes = data.results;
-            let questionIndex = 0;
-            const usedQuestions = new Set();
+    if (!quizEmAndamento) {
+        quizEmAndamento = true
+        tempoInicio = new Date().getTime() / 1000
 
-            // Função para mostrar uma pergunta
-            function showQuestion() {
-                const mostrarPergunta = document.querySelector('.container');
-                mostrarPergunta.innerHTML = '';
+        function atualizarTempo() {
+            const tempoAtual = new Date().getTime() / 1000
+            tempoPassado = Math.round(tempoAtual - tempoInicio)
+            document.getElementById('tempoPassado').textContent = tempoPassado
+        }
 
-                // Escolher uma pergunta aleatória que não foi usada
-                let perguntaAleatoria;
-                do {
-                    perguntaAleatoria = Math.floor(Math.random() * questoes.length);
-                } while (usedQuestions.has(perguntaAleatoria));
+        timerId = setInterval(atualizarTempo, 1000)
 
-                const currentQuestion = questoes[perguntaAleatoria];
-                usedQuestions.add(perguntaAleatoria);
-
-                // Mostrar a pergunta
-                const questaoElement = document.createElement('div');
-                questaoElement.classList.add('questao');
-                questaoElement.innerText = currentQuestion.question;
-                mostrarPergunta.appendChild(questaoElement);
-
-                // Mostrar opções de resposta
-                const opcoes = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer];
-                opcoes.sort(() => Math.random() - 0.5); // Embaralhar as opções
-                opcoes.forEach(opcao => {
-                    const optionElement = document.createElement('button');
-                    optionElement.classList.add('option');
-                    optionElement.innerText = opcao;
-                    optionElement.addEventListener('click', () => checarResposta(opcao, currentQuestion.correct_answer));
-                    mostrarPergunta.appendChild(optionElement);
-                });
-
-                // Mostrar pontuação abaixo da pergunta
-                const pontuacaoElement = document.createElement('div');
-                pontuacaoElement.classList.add('pontuacao');
-                pontuacaoElement.innerHTML = `Pontuação: <span id="pontuacaoAtual">${pontuacao}</span>`;
-                mostrarPergunta.appendChild(pontuacaoElement);
-            }
-
-            // Função para verificar a resposta
-            function checarResposta(selecionarResposta, respostaCorreta) {
-                const feedbackElement = document.createElement('div');
-                feedbackElement.classList.add('feedback');
-
-                if (selecionarResposta === respostaCorreta) {
-                    feedbackElement.innerText = 'Boa!! Resposta correta!';
-                    feedbackElement.style.color = 'green';
-
-                    // Atualizar pontuação abaixo da pergunta
-                    pontuacao += 1;
-                    const pontuacaoAtualElement = document.getElementById('pontuacaoAtual');
-                    pontuacaoAtualElement.innerText = pontuacao;
-                } else {
-                    feedbackElement.innerText = 'Resposta incorreta. A próxima você acerta, não desista!';
-                    feedbackElement.style.color = 'red';
-                }
-
-                const questionContainer = document.querySelector('.container');
-                questionContainer.appendChild(feedbackElement);
-
-                // Passar para a próxima pergunta após um breve intervalo 
-                setTimeout(() => {
-                    // Verificar se ainda há perguntas
-                    if (questionIndex < questoes.length - 1) {
-                        questionIndex++;
-                        showQuestion();
-                    } else {
-                        // Mensagem de fim do quiz
-                        mostrarResultadoFinal();
-                    }
-                }, 2000); // Quanto tempo a mensagem de feedback é exibida (em milissegundos)
-            }
-
-            // Função para mostrar o resultado final
-            function mostrarResultadoFinal() {
-                const resultadoFinalElement = document.getElementById('resultadoFinal');
-                resultadoFinalElement.innerText = `Você acertou ${pontuacao} de ${questoes.length} perguntas!`;
-            }
-
-            // Adicionar um event listener ao botão para iniciar o quiz
-            const iniciarQuiz = document.getElementById('startQuizButton');
-            iniciarQuiz.addEventListener('click', showQuestion);
-        });
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+            .then(response => response.json())
+            .then(data => {
+                questoes = data.results
+                showQuestion()
+            })
+    }
 }
 
-// Adicionar um event listener ao carregar a página para iniciar o quiz
-document.addEventListener('DOMContentLoaded', startQuiz);
+function showQuestion() {
+    if (questionIndex >= questoes.length) {
+        finalizarQuiz()
+        return
+    }
+
+    let perguntaAleatoria
+    do {
+        perguntaAleatoria = Math.floor(Math.random() * questoes.length)
+    } while (usedQuestions.has(perguntaAleatoria))
+
+    const currentQuestion = questoes[perguntaAleatoria]
+    usedQuestions.add(perguntaAleatoria)
+
+    mostrarPergunta.innerHTML = `
+    <div class="questao">${currentQuestion.question}</div>
+    ${currentQuestion.incorrect_answers.concat(currentQuestion.correct_answer)
+      .sort(() => Math.random() - 0.5)
+      .map(opcao => `
+        <button class="option">${opcao}</button>
+      `).join('')}
+    <div class="pontuacao">Pontuação: <span id="pontuacaoAtual">${pontuacao}</span></div>
+    <div class="tempo">Tempo: <span id="tempoPassado">${tempoPassado}</span> segundos</div>`
+
+  document.querySelectorAll('.option').forEach(option => {
+    option.addEventListener('click', () => checarResposta(option.innerText, currentQuestion.correct_answer))
+  })
+}
+
+function checarResposta(selecionarResposta, respostaCorreta) {
+  const feedbackElement = document.createElement('div')
+  feedbackElement.classList.add('feedback')
+
+  if (selecionarResposta === respostaCorreta) {
+    feedbackElement.innerText = 'Boa!! Resposta correta!'
+    feedbackElement.style.color = 'green'
+    pontuacao += 1
+  } else {
+    feedbackElement.innerText = 'Resposta incorreta. A próxima você acerta, não desista!'
+    feedbackElement.style.color = 'red'
+  }
+
+  mostrarPergunta.appendChild(feedbackElement)
+
+  setTimeout(() => {
+    questionIndex++
+    showQuestion()
+  }, 2000)
+}
+
+function finalizarQuiz() {
+  quizEmAndamento = false
+  clearInterval(timerId)
+  mostrarResultadoFinal()
+}
+
+function mostrarResultadoFinal() {
+  mostrarPergunta.innerHTML = `<div class="resultado-final">Você acertou ${pontuacao} de ${questoes.length} perguntas em ${tempoPassado} segundos!</div>`
+}
+
+document.getElementById('startQuizButton').addEventListener('click', startQuiz)
